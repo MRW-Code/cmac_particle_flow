d9be38d23c42c4213553d168dc1f83089a9ae16e
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2021-11-19 12:59:09 +0000

    Update ttv_main.py

--- ttv_main.py
+++ ttv_main.py
@@ -16,6 +16,8 @@ def empty_file(path):
 empty_file('./aug_images')
 empty_file('./split_test_images')
 
+# change the second number to the appropriate split index
+
 prep = FastAIPrep('./images', 0, 2, './aug_images', './split_test_images')
 prep.check_test_train_data()
 df = prep.get_fastai_df()
@@ -39,26 +41,23 @@ dls = ImageDataLoaders.from_df(df,
 
 learn = cnn_learner(dls, resnet18, metrics=[accuracy]).to_fp16()
 
-learn.fine_tune(200, 0.00001, cbs=[SaveModelCallback(fname='./best_cbs_200'),
+learn.fine_tune(100, 0.00001, cbs=[SaveModelCallback(fname='./best_cbs_100'),
                                   ReduceLROnPlateau(monitor='valid_loss',
                                                     min_delta=0.1,
                                                     patience=2)])
 
-
+# for training across multiple gpus
 # with learn.distrib_ctx(sync_bn=False): learn.fine_tune(100, 0.0001, cbs=[SaveModelCallback(fname='./best_cbs'),
 #                                  ReduceLROnPlateau(monitor='valid_loss',
 #                                                    min_delta=0.1, patience=2)])
 
 
-learn.export('./code_saves/trained_model_200.pkl')
+learn.export('./code_saves/trained_model_100.pkl')
 
 learn.recorder.plot_loss()
-plt.savefig('./code_saves/training_plot_200.png')
+plt.savefig('./code_saves/training_plot_100.png')
 
 interp = ClassificationInterpretation.from_learner(learn)
 interp.plot_confusion_matrix()
-plt.savefig('./conf_mtrx_200')
+plt.savefig('./conf_mtrx_100')
 
-
-# need to load the test set separately if it all works ok.
-# Also make the image dirs empty if doing the cv shuffle
