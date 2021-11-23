778fb6dce4e9c69bd4e8ec7e4c792888c0efb788
Matthew Wilkinson <mattwilkinson16@hotmail.co.uk>
2021-11-23 10:38:37 +0000

    inference has confusion matrix

--- do_inference.py
+++ do_inference.py
@@ -1,7 +1,7 @@
 from inference.inference import Inference
 import os
-from sklearn.metrics import accuracy_score
-
+from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay, confusion_matrix
+import matplotlib.pyplot as plt
 
 learner = './code_saves/trained_model_100.pkl'
 split_factor = 2
@@ -11,5 +11,8 @@ true, preds = inf.infer()
 acc = accuracy_score(true, preds)
 print(acc)
 
-
+cm = confusion_matrix(true, preds)
+disp = ConfusionMatrixDisplay(cm, display_labels=os.listdir('./external_test_set'))
+disp.plot()
+plt.savefig('./code_saves/ext_test_conf_mtrx.png')
 print('done')
