8ac8e801c2d80a96c8955ac993d4e8bf8c3fd393
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2022-01-24 09:44:00 +0000

    regression framework - beta

--- regression/do_inf_reg.py
+++ regression/do_inf_reg.py
@@ -1,6 +1,6 @@
 from inference.inference import Inference
 import os
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import matplotlib.pyplot as plt
 
 learner = '../code_saves/trained_model_200.pkl'
@@ -10,4 +10,5 @@ inf = Inference(learner, split_factor)
 true, preds = inf.infer()
 r2 = r2_score(true, preds)
 mse = mean_squared_error(true, preds)
-print(f'R2 Score = {r2} and MSE = {mse}')
+mae = mean_absolute_error(true, preds)
+print(f'R2 Score = {r2} and MSE = {mse} and MAE = {mae}')
