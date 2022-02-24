f0ffabf265d5bc637956f43775d82a989ff8ae20
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2022-02-24 13:13:17 +0000

    bug fix

--- regression/relabel_main.py
+++ regression/relabel_main.py
@@ -1,5 +1,5 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 import torch
 import glob
 torch.cuda.empty_cache()
@@ -45,42 +45,41 @@ ez_fr = 10
 df = relabel(co_ez, ez_fr, df)
 df.to_csv('input_and_labels')
 
-# tfms = None
-# dls = ImageDataLoaders.from_df(df,
-#                                seed=None,
-#                                fn_col=0,
-#                                folder=None,
-#                                suff='',
-#                                label_col=2,
-#                                label_delim=None,
-#                                valid_col=1,
-#                                item_tfms=None,
-#                                batch_tfms=tfms,
-#                                bs=16,
-#                                val_bs=None,
-#                                shuffle=True,
-#                                device=None,
-#                                y_block=RegressionBlock)
-#
-# learn = cnn_learner(dls, resnet18, metrics=[accuracy]).to_fp16()
-#
-# learn.fine_tune(100, 0.00001, cbs=[SaveModelCallback(fname='./dump/best_cbs_100'),
-#                                   ReduceLROnPlateau(monitor='valid_loss',
-#                                                     min_delta=0.1,
-#                                                     patience=2)])
-#
-# # for training across multiple gpus
-# # with learn.distrib_ctx(sync_bn=False): learn.fine_tune(100, 0.0001, cbs=[SaveModelCallback(fname='./best_cbs'),
-# #                                  ReduceLROnPlateau(monitor='valid_loss',
-# #                                                    min_delta=0.1, patience=2)])
-#
-#
-# learn.export('./dump/trained_model_100.pkl')
-#
-# learn.recorder.plot_loss()
-# plt.savefig('./dump/training_plot_100.png')
-#
-# interp = ClassificationInterpretation.from_learner(learn)
-# interp.plot_confusion_matrix()
-# plt.savefig('./dump/conf_mtrx_100.png')
+tfms = None
+dls = ImageDataLoaders.from_df(df,
+                               seed=None,
+                               fn_col=0,
+                               folder=None,
+                               suff='',
+                               label_col=2,
+                               label_delim=None,
+                               valid_col=1,
+                               item_tfms=None,
+                               batch_tfms=None,
+                               bs=16,
+                               val_bs=None,
+                               shuffle=True,
+                               device=None)
+
+learn = cnn_learner(dls, resnet18, metrics=[accuracy]).to_fp16()
+
+learn.fine_tune(100, 0.00001, cbs=[SaveModelCallback(fname='./dump/best_cbs_100'),
+                                  ReduceLROnPlateau(monitor='valid_loss',
+                                                    min_delta=0.1,
+                                                    patience=2)])
+
+# for training across multiple gpus
+# with learn.distrib_ctx(sync_bn=False): learn.fine_tune(100, 0.0001, cbs=[SaveModelCallback(fname='./best_cbs'),
+#                                  ReduceLROnPlateau(monitor='valid_loss',
+#                                                    min_delta=0.1, patience=2)])
+
+
+learn.export('./dump/trained_model_100.pkl')
+
+learn.recorder.plot_loss()
+plt.savefig('./dump/training_plot_100.png')
+
+interp = ClassificationInterpretation.from_learner(learn)
+interp.plot_confusion_matrix()
+plt.savefig('./dump/conf_mtrx_100.png')
 
