bc8e7dedbf250a1fbc3713ef45ce7e62d2cde91c
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2022-02-24 18:19:45 +0000

    bug fix

--- regression/exp.py
+++ regression/exp.py
@@ -110,7 +110,7 @@ def run(idx_list):
 
         inf = Inference(learner, split_factor)
         true, preds = inf.infer()
-        true = df.new.values()
+        true = list(df.new)
         print(f'True = {true}')
         acc = accuracy_score(true, preds)
         print(f'Accuracy = {acc}')
