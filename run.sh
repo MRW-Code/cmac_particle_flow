91c7d1c9cdd89088de3b3ebd9cc802ef4f7bf14f
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2022-06-20 14:12:54 +0100

    bug fixes

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-python3 main.py --gpu_idx 3 -s 1 2>&1 | tee ./split_factor_tests/split_factor_1.txt
-python3 main.py --gpu_idx 3 -s 2 2>&1 | tee ./split_factor_tests/split_factor_2.txt
-python3 main.py --gpu_idx 3 -s 3 2>&1 | tee ./split_factor_tests/split_factor_3.txt
-python3 main.py --gpu_idx 3 -s 4 2>&1 | tee ./split_factor_tests/split_factor_4.txt
-python3 main.py --gpu_idx 3 -s 5 2>&1 | tee ./split_factor_tests/split_factor_5.txt
-python3 main.py --gpu_idx 3 -s 6 2>&1 | tee ./split_factor_tests/split_factor_6.txt
-python3 main.py --gpu_idx 3 -s 7 2>&1 | tee ./split_factor_tests/split_factor_7.txt
-python3 main.py --gpu_idx 3 -s 8 2>&1 | tee ./split_factor_tests/split_factor_8.txt
-python3 main.py --gpu_idx 3 -s 9 2>&1 | tee ./split_factor_tests/split_factor_9.txt
-python3 main.py --gpu_idx 3 -s 10 2>&1 | tee ./split_factor_tests/split_factor_10.txt
+python3 main.py --gpu_idx 3 -s 1 --from_scratch 2>&1 | tee ./split_factor_tests/split_factor_1.txt
+python3 main.py --gpu_idx 3 -s 2 --from_scratch 2>&1 | tee ./split_factor_tests/split_factor_2.txt
+python3 main.py --gpu_idx 3 -s 3 --from_scratch 2>&1 | tee ./split_factor_tests/split_factor_3.txt
+python3 main.py --gpu_idx 3 -s 4 --from_scratch 2>&1 | tee ./split_factor_tests/split_factor_4.txt
+python3 main.py --gpu_idx 3 -s 5 --from_scratch 2>&1 | tee ./split_factor_tests/split_factor_5.txt
+python3 main.py --gpu_idx 3 -s 6 --from_scratch 2>&1 | tee ./split_factor_tests/split_factor_6.txt
+python3 main.py --gpu_idx 3 -s 7 --from_scratch 2>&1 | tee ./split_factor_tests/split_factor_7.txt
+python3 main.py --gpu_idx 3 -s 8 --from_scratch 2>&1 | tee ./split_factor_tests/split_factor_8.txt
+python3 main.py --gpu_idx 3 -s 9 --from_scratch 2>&1 | tee ./split_factor_tests/split_factor_9.txt
+python3 main.py --gpu_idx 3 -s 10 --from_scratch 2>&1 | tee ./split_factor_tests/split_factor_10.txt
 
 
