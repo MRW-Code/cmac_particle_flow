8ae2b936a5f12d23cce64d63ba318ae37c0d7a50
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2022-07-11 14:26:09 +0100

    bux fix

--- src/inference.py
+++ src/inference.py
@@ -41,7 +41,7 @@ class Inference:
                 crops.append(img[points_x[i]:points_x[i] + points_x[1], points_y[j]:points_y[j] + points_y[1]])
 
 
-        crops = [torch.tensor(crop).clone().detach() for crop in crops]
+        crops = [torch.tensor(crop).cpu() for crop in crops]
         return crops
 
     def preprocess_split(self):
