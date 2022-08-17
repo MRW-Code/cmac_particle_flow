59162dcd96b7cedd7193b5a8cbc50f45f2880249
Matthew Wilkinson <mattwilkinson16@hotmail.co.uk>
2022-08-17 15:54:02 +0100

    ttv_kfold

--- src/training.py
+++ src/training.py
@@ -32,13 +32,13 @@ def train_fastai_model_classification(model_df, count, exp_type):
                                 ReduceLROnPlateau(monitor='valid_loss',
                                                   min_delta=0.05,
                                                   patience=2),
-                                 EarlyStoppingCallback(monitor='accuracy', min_delta=0.1, patience=20)])
+                                 EarlyStoppingCallback(monitor='accuracy', min_delta=0.1, patience=10)])
     else:
         learn.fine_tune(100, cbs=[SaveModelCallback(monitor='valid_loss', fname=f'./csd_{args.no_augs}_best_cbs.pth'),
                                 ReduceLROnPlateau(monitor='valid_loss',
                                                   min_delta=0.05,
                                                   patience=2),
-                                 EarlyStoppingCallback(monitor='accuracy', min_delta=0.1, patience=20),
+                                 EarlyStoppingCallback(monitor='accuracy', min_delta=0.1, patience=10),
                                   GradientAccumulation(n_acc=args.grad_accum)])
 
     # print(learn.validate())
