4a12d7d0db64402900b5e9af53e2603f0313ad32
Matthew Wilkinson <mattwilkinson16@hotmail.co.uk>
2022-08-27 09:18:18 +0100

    fix data leak

--- runner.sh
+++ runner.sh
@@ -1,34 +1,68 @@
 #!/bin/bash
 
-## Testing the different models for sf = 10
-mkdir ./model_testing_leak
-python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m resnet18 --split_factor 10 2>&1 | tee ./model_testing_leak/resnet18_sf_10.txt
-python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m convnext_tiny --split_factor 10 2>&1 | tee ./model_testing_leak/convnext_tiny_sf_10.txt
-mkdir ./model_testing_repeat_1_leak
-python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m resnet18 --split_factor 10 2>&1 | tee ./model_testing_repeat_1_leak/resnet18_sf_10.txt
-python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m convnext_tiny --split_factor 10 2>&1 | tee ./model_testing_repeat_1_leak/convnext_tiny_sf_10.txt
-mkdir ./model_testing_repeat_2_leak
-python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m resnet18 --split_factor 10 2>&1 | tee ./model_testing_repeat_2_leak/resnet18_sf_10.txt
-python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m convnext_tiny --split_factor 10 2>&1 | tee ./model_testing_repeat_2_leak/convnext_tiny_sf_10.txt
+### Testing the different models for sf = 10
+#mkdir ./model_testing_leak
+#python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m resnet18 --split_factor 10 2>&1 | tee ./model_testing_leak/resnet18_sf_10.txt
+#python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m convnext_tiny --split_factor 10 2>&1 | tee ./model_testing_leak/convnext_tiny_sf_10.txt
+#mkdir ./model_testing_repeat_1_leak
+#python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m resnet18 --split_factor 10 2>&1 | tee ./model_testing_repeat_1_leak/resnet18_sf_10.txt
+#python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m convnext_tiny --split_factor 10 2>&1 | tee ./model_testing_repeat_1_leak/convnext_tiny_sf_10.txt
+#mkdir ./model_testing_repeat_2_leak
+#python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m resnet18 --split_factor 10 2>&1 | tee ./model_testing_repeat_2_leak/resnet18_sf_10.txt
+#python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m convnext_tiny --split_factor 10 2>&1 | tee ./model_testing_repeat_2_leak/convnext_tiny_sf_10.txt
+#
+### Testing the resnet split factors
+#mkdir ./resnet_split_factor_leak
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 2 --no_resize  2>&1 | tee ./resnet_split_factor_leak/split_factor_2.txt
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 4 --no_resize  2>&1 | tee ./resnet_split_factor_leak/split_factor_4.txt
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 6 --no_resize  2>&1 | tee ./resnet_split_factor_leak/split_factor_6.txt
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 8 --no_resize  2>&1 | tee ./resnet_split_factor_leak/split_factor_8.txt
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 10 --no_resize 2>&1 | tee ./resnet_split_factor_leak/split_factor_10.txt
+#mkdir ./resnet_split_factor_repeat1_leak
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 2 --no_resize  2>&1 | tee ./resnet_split_factor_repeat1_leak/split_factor_2.txt
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 4 --no_resize  2>&1 | tee ./resnet_split_factor_repeat1_leak/split_factor_4.txt
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 6 --no_resize  2>&1 | tee ./resnet_split_factor_repeat1_leak/split_factor_6.txt
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 8 --no_resize  2>&1 | tee ./resnet_split_factor_repeat1_leak/split_factor_8.txt
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 10 --no_resize 2>&1 | tee ./resnet_split_factor_repeat1_leak/split_factor_10.txt
+#mkdir ./resnet_split_factor_repeat2_leak
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 2 --no_resize  2>&1 | tee ./resnet_split_factor_repeat2_leak/split_factor_2.txt
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 4 --no_resize  2>&1 | tee ./resnet_split_factor_repeat2_leak/split_factor_4.txt
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 6 --no_resize  2>&1 | tee ./resnet_split_factor_repeat2_leak/split_factor_6.txt
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 8 --no_resize  2>&1 | tee ./resnet_split_factor_repeat2_leak/split_factor_8.txt
+#python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 10 --no_resize 2>&1 | tee ./resnet_split_factor_repeat2_leak/split_factor_10.txt
 
-## Testing the resnet split factors
-mkdir ./resnet_split_factor_leak
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 2 --no_resize  2>&1 | tee ./resnet_split_factor_leak/split_factor_2.txt
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 4 --no_resize  2>&1 | tee ./resnet_split_factor_leak/split_factor_4.txt
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 6 --no_resize  2>&1 | tee ./resnet_split_factor_leak/split_factor_6.txt
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 8 --no_resize  2>&1 | tee ./resnet_split_factor_leak/split_factor_8.txt
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 10 --no_resize 2>&1 | tee ./resnet_split_factor_leak/split_factor_10.txt
-mkdir ./resnet_split_factor_repeat1_leak
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 2 --no_resize  2>&1 | tee ./resnet_split_factor_repeat1_leak/split_factor_2.txt
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 4 --no_resize  2>&1 | tee ./resnet_split_factor_repeat1_leak/split_factor_4.txt
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 6 --no_resize  2>&1 | tee ./resnet_split_factor_repeat1_leak/split_factor_6.txt
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 8 --no_resize  2>&1 | tee ./resnet_split_factor_repeat1_leak/split_factor_8.txt
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 10 --no_resize 2>&1 | tee ./resnet_split_factor_repeat1_leak/split_factor_10.txt
-mkdir ./resnet_split_factor_repeat2_leak
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 2 --no_resize  2>&1 | tee ./resnet_split_factor_repeat2_leak/split_factor_2.txt
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 4 --no_resize  2>&1 | tee ./resnet_split_factor_repeat2_leak/split_factor_4.txt
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 6 --no_resize  2>&1 | tee ./resnet_split_factor_repeat2_leak/split_factor_6.txt
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 8 --no_resize  2>&1 | tee ./resnet_split_factor_repeat2_leak/split_factor_8.txt
-python3 main.py -b 8 -v kfold_ttv --from_scratch -m resnet18 --split_factor 10 --no_resize 2>&1 | tee ./resnet_split_factor_repeat2_leak/split_factor_10.txt
+mkdir ./vit_out_new_leak
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 1 2>&1 | tee ./vit_out_new_leak/split_factor_1.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 2 2>&1 | tee ./vit_out_new_leak/split_factor_2.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 3 2>&1 | tee ./vit_out_new_leak/split_factor_3.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 4 2>&1 | tee ./vit_out_new_leak/split_factor_4.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 5 2>&1 | tee ./vit_out_new_leak/split_factor_5.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 6 2>&1 | tee ./vit_out_new_leak/split_factor_6.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 7 2>&1 | tee ./vit_out_new_leak/split_factor_7.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 8 2>&1 | tee ./vit_out_new_leak/split_factor_8.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 9 2>&1 | tee ./vit_out_new_leak/split_factor_9.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 10 2>&1 | tee ./vit_out_new_leak/split_factor_10.txt
 
+mkdir ./vit_out_new_leak_repeat_leak
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 1 2>&1 | tee ./vit_out_new_leak_repeat_leak/split_factor_1.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 2 2>&1 | tee ./vit_out_new_leak_repeat_leak/split_factor_2.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 3 2>&1 | tee ./vit_out_new_leak_repeat_leak/split_factor_3.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 4 2>&1 | tee ./vit_out_new_leak_repeat_leak/split_factor_4.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 5 2>&1 | tee ./vit_out_new_leak_repeat_leak/split_factor_5.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 6 2>&1 | tee ./vit_out_new_leak_repeat_leak/split_factor_6.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 7 2>&1 | tee ./vit_out_new_leak_repeat_leak/split_factor_7.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 8 2>&1 | tee ./vit_out_new_leak_repeat_leak/split_factor_8.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 9 2>&1 | tee ./vit_out_new_leak_repeat_leak/split_factor_9.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 10 2>&1 | tee ./vit_out_new_leak_repeat_leak/split_factor_10.txt
 
+mkdir ./vit_out_new_leak_repeat_leak_2
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 1 2>&1 | tee ./vit_out_new_leak_repeat_leak_2/split_factor_1.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 2 2>&1 | tee ./vit_out_new_leak_repeat_leak_2/split_factor_2.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 3 2>&1 | tee ./vit_out_new_leak_repeat_leak_2/split_factor_3.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 4 2>&1 | tee ./vit_out_new_leak_repeat_leak_2/split_factor_4.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 5 2>&1 | tee ./vit_out_new_leak_repeat_leak_2/split_factor_5.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 6 2>&1 | tee ./vit_out_new_leak_repeat_leak_2/split_factor_6.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 7 2>&1 | tee ./vit_out_new_leak_repeat_leak_2/split_factor_7.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 8 2>&1 | tee ./vit_out_new_leak_repeat_leak_2/split_factor_8.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 9 2>&1 | tee ./vit_out_new_leak_repeat_leak_2/split_factor_9.txt
+python3 main.py -b 32 -v kfold_ttv --from_scratch --make_figs -m vit_tiny_patch16_384 --split_factor 10 2>&1 | tee ./vit_out_new_leak_repeat_leak_2/split_factor_10.txt
